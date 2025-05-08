3a9fc2fee629b05d6c409d08e1d54f099fe713c7
AstaCarl <cph-ac300@cphbusiness.dk>
2025-05-08 21:47:33 +0200

    added a select input field

---
+++ washworld-frontend/components/SelectInput.tsx
@@ -0,0 +1,106 @@
+import { Label } from "@react-navigation/elements";
+import { View, Text } from "react-native";
+import { StyleSheet } from "react-native";
+import React from "react";
+import SelectDropdown from "react-native-select-dropdown";
+import ChevronIcon from "./icons/ChevronIcon";
+
+type selectInputProps = {
+  labelText: string;
+};
+
+export default function SelectInput({ labelText }: selectInputProps) {
+  const testData = [
+    { id: "1", name: "Item 1" },
+    { id: "2", name: "Item 2" },
+    { id: "3", name: "Item 3" },
+    { id: "4", name: "Item 4" },
+  ];
+
+  return (
+    <View style={styles.inputGroup}>
+      <Label style={styles.label}>{labelText}</Label>
+      <SelectDropdown
+        data={testData}
+        onSelect={(selectedItem) => {
+          console.log("Selected item:", selectedItem);
+        }}
+        renderButton={(selectedItem) => {
+          return (
+            <View style={styles.input}>
+              <Text style={styles.dropdownButtonTxtStyle}>
+                {selectedItem ? selectedItem.name : "select a location"}
+              </Text>
+            </View>
+          );
+        }}
+        renderItem={(item, isSelected) => {
+          return (
+            <View
+              style={{
+                ...styles.dropdownItemStyle,
+                ...(isSelected && { backgroundColor: "#f7f7f7" }),
+              }}
+            >
+              <Text style={styles.dropdownItemTxtStyle}>{item.name}</Text>
+            </View>
+          );
+        }}
+        showsVerticalScrollIndicator={false}
+        dropdownStyle={styles.dropdownMenuStyle}
+      />
+      <View style={styles.icon}>
+        <ChevronIcon color="#06C167" />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  label: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#000",
+  },
+  inputGroup: {
+    display: "flex",
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+    gap: 5,
+    width: "100%",
+    position: "relative",
+  },
+  input: {
+    width: "100%",
+    height: 60,
+    backgroundColor: "#F7F7F7",
+    borderBottomColor: "#E5E5E5",
+    borderBottomWidth: 2,
+    paddingHorizontal: 12,
+    justifyContent: "center",
+  },
+  dropdownButtonTxtStyle: {
+    fontSize: 14,
+    color: "#666666",
+  },
+  dropdownMenuStyle: {
+    backgroundColor: "#f7f7f7",
+  },
+  dropdownItemStyle: {
+    width: "100%",
+    flexDirection: "row",
+    paddingHorizontal: 12,
+    paddingVertical: 8,
+  },
+  dropdownItemTxtStyle: {
+    flex: 1,
+    fontSize: 18,
+    fontWeight: "500",
+    color: "#333333",
+  },
+  icon: {
+    position: "absolute",
+    right: 12,
+    top: 48,
+  },
+});
--- washworld-frontend/screens/HomeScreen.jsx
+++ washworld-frontend/screens/HomeScreen.jsx
@@ -1,15 +1,12 @@
 import { Text } from "@react-navigation/elements";
 import { StyleSheet, View } from "react-native";
 import React from 'react';
-import TextInput from "../components/TextInput";
 
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
       <Text>Home</Text>
-      <TextInput labelText="4. Your E-mail" placeholderText="Email address"/>
-      <TextInput labelText="5. Your Password" placeholderText="Password" password={true}/>
     </View>
   );
 }
