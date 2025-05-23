b77edb00cc77508641262ceca5b04db0496b1515
AstaCarl <cph-ac300@cphbusiness.dk>
2025-05-23 12:33:54 +0200

    swagger implementation

--- washworld-backend/src/main.ts
+++ washworld-backend/src/main.ts
@@ -1,14 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-const cors = require("cors");
-app.use(cors());
+  const cors = require('cors');
+  app.use(cors());
   app.enableCors({
     origin: '*', // Replace with your frontend URL
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
+
+  const configb = new DocumentBuilder()
+    .setTitle('WashWorld API')
+    .setDescription('The WashWorld API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, configb);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
